1ad120fe6aeb8eb45e1d9cb602e3f7fee1006991
berlinpadang18 <45735600+berlinpadang18@users.noreply.github.com>
2021-03-16 15:32:40 +0700

    SKRIPSI SUUU

--- the_ONE_Simulator-master/routing/SoSim.java
+++ the_ONE_Simulator-master/routing/SoSim.java
@@ -11,7 +11,6 @@ import core.Message;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import report.Report;
 
 /**
  *
@@ -28,7 +27,6 @@ public class SoSim implements RoutingDecisionEngine{
 //            
 //        }
         this.euclideanSim = hitungEuclideanSim(thisHost, peer);
-
     }
 
     @Override
