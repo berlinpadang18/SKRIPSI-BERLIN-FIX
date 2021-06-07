ac63143d4fa4731eea44c19ba98ebb29613abd6c
berlinpadang18 <45735600+berlinpadang18@users.noreply.github.com>
2021-06-07 17:29:29 +0700

    Merge origin/master
    Conflicts:
    	the_ONE_Simulator-master/SKRIPSI.txt
    	the_ONE_Simulator-master/routing/SoSim.java

--- the_ONE_Simulator-master/routing/BantuHitung.java
+++ the_ONE_Simulator-master/routing/BantuHitung.java
@@ -12,22 +12,19 @@ package routing;
 public class BantuHitung {
 
     public BantuHitung() {
-        
+
     }
-    
-    public double pembagi(int x, int y){
-        return (double) x/ (double) y;
+
+    public double pembagi(int x, int y) {
+        return (double) x / (double) y;
     }
-    
-    public double hitungtanimoto (double xy, double x, double y){
+
+    public double hitungtanimoto(double xy, double x, double y) {
 //        System.out.println(( a   /  (b + c - a)));
-        return ( xy / (x + y - xy)) ;
+        return (xy / (x + y - xy));
     }
-    public double hitungEuclidean(double a, double b){
+
+    public double hitungEuclidean(double a, double b) {
         return 1 - (Math.sqrt(a) / (Math.sqrt(b)));
-    }
-    
-    public double kali(double a, double b){
-        return a * b;
     }
 }
